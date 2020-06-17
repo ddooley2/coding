59fb0703db51a30cfc0df603993aa680725e59ba
David Dooley <dools.d@gmail.com>
2020-06-16 23:00:28 -0400

    OD up and running, add legend, axis modification later

--- data_analysis/OD.py
+++ data_analysis/OD.py
@@ -2,46 +2,62 @@ import pandas as pd
 import easygui as eg
 import datetime as dt
 import time
+import matplotlib.pyplot as plt
 
+
+###Import data and perform initial trims (must be .xlsx file)
 file_path = eg.fileopenbox(msg="Please select an Excel file with raw kinetic OD data")
 df = pd.read_excel(file_path,header=26) #Save Excel data in dataframe
 cut_row = df.index.get_loc(df.index[df['Unnamed: 0'] == "Results"][0]) - 2 #Trim Results from dataframe
 df = df.iloc[:cut_row,1:] #Cut dataframe to region of data
 df = df.dropna(how='all',axis='columns') #Remove columns with no date
 
-#Convert time to decimal format (hrs)
+
+###Convert time to decimal hours format
 for i,time in enumerate(df['Time']):
     df['Time'].iloc[i] = float(time.hour + (time.minute/60))
 
-#Enter blank columns
+
+###Enter blank columns
 b = input("\nEnter any blank columns in a comma-separated list: ")
 blanks = []
 for ind in b.split(","):
     blanks.append(ind.strip())
 
-#Remove blank columns
+###Remove blank columns
 for col in blanks:
     df = df.drop([col], axis='columns')
 
-#Enter control columns
+
+###Enter control columns
 c = input("\nEnter any control columns in a comma-separated list: ")
 controls = []
 for ind in c.split(","):
     controls.append(ind.strip())
 
-#Enter data columns 
-#d = input("\nEnter any blank columns in a comma-separated list: ")
-#data = []
-#for ind in d.split(","):
-#    data.append(ind.strip())
 
+###Enter data series
+series_bool = True
+names=[]
+while series_bool:
+    ans = str(input("\nWould you like to add a data series? "))
+    if (ans == "yes" or ans == "y" or ans == "Y" or ans == "Yes" or ans == "YES"):
+        data = []
+        name = input("\nEnter the name for this data series: ")
+        d = input("\nEnter the column(s) containing OD data for this series (separated by commas if in replicates): ")
+        for ind in d.split(","):
+            data.append(ind.strip())
+        if len(d) > 1:
+            df[name+'_avg']=df.loc[:,data].mean(axis=1) #Average duplicates, triplicates, etc.
+            names.append(name+'_avg') #Save name of series to list
+        else:
+            df[name+'_avg']=df[data]
+    else:
+        series_bool = False
 
+ax = plt.gca()
 
+df.plot(x='Time', y=names, cmap = 'tab20', ax=ax)
+df.plot(x='Time',y=controls, cmap = 'tab20b', ax=ax)
+plt.show()
 
-
-print(df)
-#print(mat['Unnamed: 0']=="Results"))
-
-#print(mat)
-#print(mat.head(10))
-#mat = mat[27,:]
